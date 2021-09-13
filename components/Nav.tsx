45e4b9df078b6548254dd323dbeb92d97689a58d
angelopoole <angelo.m.poole@gmail.com>
2021-09-12 23:46:00 -0400

    Add logic for mobile && desktop Navbar states, css inside of js would be amazing here

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,34 +1,60 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import styles from '../styles/Nav.module.scss';
 import Link from 'next/Link';
+import useOnClickOutside from '../hooks/useOnClickOutside';
 
 const Nav = () => {
+  const [navOpen, setNavOpen] = useState(false);
+  const node = useRef(null);
+
+  useOnClickOutside(node, () => setNavOpen(false));
+
+  const toggleNav = () => {
+    console.log(navOpen);
+    setNavOpen((prevState) => !prevState);
+    console.log(navOpen, 'after');
+  };
+
+  let navClassname = navOpen
+    ? `${styles.nav} ${styles.navOpen}`
+    : `${styles.nav}`;
+  console.log(navClassname, navOpen);
+
+  let ulClassname = navOpen
+    ? `${styles.navUl} ${styles.ulOpen}`
+    : `${styles.navUl}`;
+
   return (
-    <nav className={styles.nav}>
-      <ul className={styles.navUl}>
-        <li>
-          <Link href='/'>
-            <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href='/QuizSelection'>
-            <a>Quiz</a>
-          </Link>
-        </li>
-        <li>
-          <Link href='/'>
-            <a>Data</a>
-          </Link>
-        </li>
-        <li>
-          <Link href='/'>
-            <a>About</a>
-          </Link>
-        </li>
-        <div className={styles.hamburger}></div>
-      </ul>
-    </nav>
+    <>
+      <nav className={navClassname} ref={node}>
+        <button onClick={toggleNav} className={styles.openNavButton}>
+          O
+        </button>
+        <ul className={ulClassname}>
+          <li>
+            <Link href='/'>
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/QuizSelection'>
+              <a>Quiz</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/'>
+              <a>Data</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/'>
+              <a>About</a>
+            </Link>
+          </li>
+          <div className={styles.hamburger}></div>
+        </ul>
+      </nav>
+    </>
   );
 };
 
