261b2f51a3b3d1d2375962b7afcd27e13ae2097f
Torja Istiaque <torja50@hotmail.com>
2021-09-14 19:49:53 -0400

    initial quiz selection page

---
+++ components/sections/QuizCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '../../styles/QuizSelection.module.scss';
+//TODO: make a quiz selection card component import that to here and place it
+const QuizCard = (props: any) => {
+  return (
+    <div className={styles.card}>
+      <p>dude</p>
+    </div>
+  );
+};
+export default QuizCard;
--- pages/QuizSelection.tsx
+++ pages/QuizSelection.tsx
@@ -1,15 +1,13 @@
+import QuizSelectionSection from '../components/sections/QuizCard';
 import React from 'react';
+import QuizCard from '../components/sections/QuizCard';
 
 const QuizSelection = () => {
   return (
     <div>
-      <ul>
-        <li>one</li>
-        <li>two</li>
-        <li>three</li>
-        <li>four</li>
-        <li>five</li>
-      </ul>
+      <QuizCard />
+      {/* <h1>hello</h1> */}
+      {/*  */}
     </div>
   );
 };
