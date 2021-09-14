a4a6af0edd35fea2c373c3b214415541671d2ac3
angelopoole <angelo.m.poole@gmail.com>
2021-09-13 23:19:42 -0400

    add data hero basic setup, prep init chart.js

--- components/sections/DataHero.tsx
+++ components/sections/DataHero.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import styles from '../../styles/DataHero.module.scss';
 
 const DataHero = () => {
-  return <div className={styles.dataCardContainer}></div>;
+  return (
+    <div className={styles.dataCardContainer}>
+      <div className={styles.flexContainer}>
+        <div className={styles.dataGraphContainer}></div>
+        <div className={styles.dataDetailsContainer}></div>
+      </div>
+    </div>
+  );
 };
 
 export default DataHero;
