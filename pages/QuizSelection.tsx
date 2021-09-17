1164b83c0a4cf84fa04c7baa5544ed8e8e333862
Torja Istiaque <torja50@hotmail.com>
2021-09-17 14:43:49 -0400

    added buttons and gradient to background

--- pages/QuizSelection.tsx
+++ pages/QuizSelection.tsx
@@ -4,12 +4,14 @@ import styles from '../styles/QuizSelection.module.scss';
 
 const QuizSelection: React.FC = () => {
   return (
-    <div className={styles.PageContainer}>
+    <div className={styles.pageContainer}>
       <div className={styles.cardFirst}>
-        <QuizCard text='Whats good in the hood dude' />
+        <QuizCard text='Play a game based on your playlists!' />
+        <button className={styles.button + ' ' + styles.ripple}>Play</button>
       </div>
       <div className={styles.cardSecond}>
-        <QuizCard text='Whats good in the hood dude' />
+        <QuizCard text='Play a game based on your top tracks!' />
+        <button className={styles.button + ' ' + styles.ripple}>Play</button>
       </div>
     </div>
   );
