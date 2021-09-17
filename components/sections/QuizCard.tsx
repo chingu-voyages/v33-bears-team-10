f80ca80e327ee796f4253219c41a95cb205d938f
Torja Istiaque <torja50@hotmail.com>
2021-09-17 11:56:49 -0400

    quiz card changing colour functionality added

--- components/sections/QuizCard.tsx
+++ components/sections/QuizCard.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from '../../styles/QuizSelection.module.scss';
-//TODO: make a quiz selection card component import that to here and place it
-const QuizCard = (props: any) => {
+interface Props {
+  text: string;
+}
+export const QuizCard: React.FC<Props> = ({ text }) => {
   return (
-    <div className={styles.card}>
-      <p>dude</p>
+    <div>
+      <p>{text}</p>
     </div>
   );
 };
-export default QuizCard;
--- pages/QuizSelection.tsx
+++ pages/QuizSelection.tsx
@@ -1,15 +1,41 @@
-import QuizSelectionSection from '../components/sections/QuizCard';
 import React from 'react';
-import QuizCard from '../components/sections/QuizCard';
+import { QuizCard } from '../components/sections/QuizCard';
+import styles from '../styles/QuizSelection.module.scss';
 
-const QuizSelection = () => {
+const QuizSelection: React.FC = () => {
   return (
-    <div>
-      <QuizCard />
-      {/* <h1>hello</h1> */}
-      {/*  */}
+    <div className={styles.PageContainer}>
+      <div className={styles.cardFirst}>
+        <QuizCard text='Whats good in the hood dude' />
+      </div>
+      <div className={styles.cardSecond}>
+        <QuizCard text='Whats good in the hood dude' />
+      </div>
     </div>
   );
 };
 
 export default QuizSelection;
+// const example = () => {
+//   return (
+//     <div className={styles.PageContainer}>
+//       <ul>
+//         <li>
+//           <Component />
+//         </li>
+//       </ul>
+
+//       <Component />
+//       <Component />
+
+//       <div oneCard>
+//         <div catagory1>
+//           <div role='button'>topTracks</div>
+//           <div>Playlists</div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default example;
