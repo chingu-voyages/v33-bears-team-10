f99d5de844aa238402b134ebd30f9753556676e7
angelopoole <angelo.m.poole@gmail.com>
2021-09-17 23:23:04 -0400

    add comment for structure of what functions should be in the useAudio hook

---
+++ hooks/useAudio.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+/**
+ * use audio should accept a song's url to the mp3 file and return a new audio instance with controls to control that instance.
+ *
+ * eg: const song = useAudio()
+ *
+ * song.play('url here')
+ * song.stop()
+ * song.pause()
+ *
+ * If adding a queue, consider giving each song an id.
+ * song.queue('url')
+ * song.dequeue('')
+ *
+ * songs should be paused before moving onto the next song instance.
+ * songs should have an audio slider option
+ * eg:
+ * song.volume(num:number)
+ * song.mute(song.volume(0)) <- toggle?
+ *
+ */
+
+export const useAudio = () => {};
