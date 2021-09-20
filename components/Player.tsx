ded34fe9c082bf8c99b09d861db16cdbfbbca237
angelopoole <angelo.m.poole@gmail.com>
2021-09-19 21:32:02 -0400

    Create test function to test audio playback, create returnAudioFromUrl function that will return audio based on the url given to it.

---
+++ components/Player.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// test  creation of audio through react web player
+
+const Player = () => {
+  return <div></div>;
+};
+
+export default Player;
--- hooks/useAudio.tsx
+++ hooks/useAudio.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 /**
  * use audio should accept a song's url to the mp3 file and return a new audio instance with controls to control that instance.
@@ -22,20 +22,51 @@ import React, { useEffect, useRef } from 'react';
  */
 
 export const useAudio = (url: string) => {
+  const [loaded, setLoaded] = useState(false);
   let audioContext = useRef<AudioContext | null>(null);
 
   useEffect(() => {
     audioContext.current = new AudioContext();
+    setLoaded((prevState) => !prevState);
     // useEffect will ensure that the page is loaded before setting up an audio context.
   }, []);
+  console.log(audioContext.current, 'context');
 
-  console.log(audioContext);
+  // testplayaudio gives us an example of what's possible with webAudio's api
+  let testPlayAudio = () => {
+    if (!audioContext.current) {
+      return 'Error in playing audio';
+    }
 
-  let playAudio = () => {
-    console.log('hit play audio');
+    let audioCtx = audioContext.current;
+
+    let gain = audioCtx.createGain();
+    gain.gain.exponentialRampToValueAtTime(0.001, audioCtx.currentTime + 0.9);
+
+    let osc = audioCtx.createOscillator();
+    osc.type = 'triangle';
+    osc.frequency.value = 350;
+    osc.frequency.exponentialRampToValueAtTime(600, audioCtx.currentTime + 1);
+    osc.start();
+
+    osc.stop(audioCtx.currentTime + 1);
+    osc.connect(gain).connect(audioCtx.destination);
+
+    return 'finished';
   };
 
-  // let osc = audioContext
+  let returnAudioFromUrl = () => {
+    let audio = new Audio(url);
+    audio.play();
+    setTimeout(() => audio.pause(), 5000);
+  };
 
-  return <audio src={url} />;
+  console.log(audioContext.current?.state);
+
+  if (audioContext.current?.state === 'suspended') {
+    audioContext.current.resume();
+    console.log(audioContext.current?.state);
+  }
+  return loaded ? returnAudioFromUrl() : 'bungo';
+  // return <audio src={url} autoPlay />;
 };
