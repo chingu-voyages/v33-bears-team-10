27146909128bdfdae29f34ea191faa544ff7a15b
angelopoole <angelo.m.poole@gmail.com>
2021-09-22 17:57:38 -0400

    create fade in and out withing userAudio hook, delete unneded code

--- hooks/useAudioPlayer.ts
+++ hooks/useAudioPlayer.ts
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 /**
  *
@@ -7,26 +7,28 @@ import React, { useState } from 'react';
  * @returns playSong() pauseSong()
  */
 
-export const useAudioPlayer = (timer = 10, numberOfSongs = 10) => {
+export const useAudioPlayer = (userTimer = 10, numberOfSongs = 10) => {
   // If you save audio and audioContext, you'll be able to refrence and edit them at any time.
   const [currentSong, setCurrentSong] = useState<{
     audio: HTMLAudioElement;
     audioContext: AudioContext;
   } | null>();
 
+  let audioCTX: AudioContext | undefined;
+
   const pauseSong = () => {
-    if (currentSong?.audio.paused) {
-      currentSong.audio.play();
+    if (currentSong?.audioContext.state === 'suspended') {
+      currentSong.audioContext.resume();
     } else {
-      currentSong?.audio.pause();
+      currentSong?.audioContext.suspend();
     }
   };
 
   const endSong = () => {
-    currentSong?.audio.pause();
     if (currentSong?.audioContext.state !== 'closed') {
       currentSong?.audioContext?.close();
     }
+
     setCurrentSong((prevState) => null);
   };
 
@@ -39,20 +41,29 @@ export const useAudioPlayer = (timer = 10, numberOfSongs = 10) => {
 
     // create audio, allow crossOrigin on audio.
     let audio = new Audio(url);
-    audio.crossOrigin;
+    audio.crossOrigin = 'anonymous';
+
+    let track = audioContext.createMediaElementSource(audio);
+
+    let gain = audioContext.createGain();
+    gain.gain.setValueAtTime(0.001, 0);
+    gain.gain.exponentialRampToValueAtTime(0.6, audioContext.currentTime + 1);
+    gain.gain.exponentialRampToValueAtTime(0.0001, userTimer);
+
+    track.connect(gain).connect(audioContext.destination);
 
     // add audio to state to later pause or remove audio.
-    setCurrentSong({ audio, audioContext });
+
+    setCurrentSong({
+      audio,
+      audioContext,
+    });
 
     audio.play();
-
-    // wait before stopping the song, song can be stoped either at base 10 seconds
-    setTimeout(() => {
-      if (!audio.paused) {
-        pauseSong();
-      }
-    }, timer * 1000);
   };
 
+  if (currentSong?.audioContext.state === 'suspended') {
+    console.log(currentSong.audioContext.currentTime);
+  }
   return { playSong, pauseSong, endSong };
 };
