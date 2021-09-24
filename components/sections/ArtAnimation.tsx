4d785ecb0b5078de408be8b43d286869757951e8
Torja Istiaque <torja50@hotmail.com>
2021-09-24 14:22:59 -0400

    got rid of console errors by removing unused image and height properties

--- components/sections/ArtAnimation.tsx
+++ components/sections/ArtAnimation.tsx
@@ -2,53 +2,25 @@ import React from 'react';
 import Image from 'next/image';
 import styles from '../../styles/ArtAnimation.module.scss';
 interface Props {
-imageUrls: string[];
+  imageUrls: string[];
 }
-const imageSpecs = {
-  height: 200,
-  width: 200,
-};
 const ArtAnimation = ({ imageUrls }: Props) => {
   return (
     <div className={styles.flexContainer}>
       <div className={styles.img1}>
-        <Image
-          height={imageSpecs.height}
-          width={imageSpecs.width}
-          layout='fill'
-          src={imageUrls[0]}
-          alt='album cover 1'
-        />
+        <Image layout='fill' src={imageUrls[0]} alt='album cover 1' />
       </div>
 
       <div className={styles.img2}>
-        <Image
-          height={imageSpecs.height}
-          width={imageSpecs.width}
-          layout='fill'
-          src={imageUrls[1]}
-          alt='album cover 2'
-        />
+        <Image layout='fill' src={imageUrls[1]} alt='album cover 2' />
       </div>
 
       <div className={styles.img3}>
-        <Image
-          height={imageSpecs.height}
-          width={imageSpecs.width}
-          layout='fill'
-          src={imageUrls[2]}
-          alt='album cover 3'
-        />
+        <Image layout='fill' src={imageUrls[2]} alt='album cover 3' />
       </div>
 
       <div className={styles.img4}>
-        <Image
-          height={imageSpecs.height}
-          width={imageSpecs.width}
-          layout='fill'
-          src={imageUrls[3]}
-          alt='album cover 4'
-        />
+        <Image layout='fill' src={imageUrls[3]} alt='album cover 4' />
       </div>
     </div>
   );
