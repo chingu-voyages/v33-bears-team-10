c61f85c7ab0d6fa15b062c27fefcf6bc30aedfb6
angelopoole <angelo.m.poole@gmail.com>
2021-09-26 20:27:38 -0400

    update TestPage tests

--- pages/TestPage.tsx
+++ pages/TestPage.tsx
@@ -4,7 +4,7 @@ import { useAuth } from '../hooks/useAuth';
 
 const TestPage = () => {
   const testAudio =
-    'https://p.scdn.co/mp3-preview/de1bf03287866de45384bd67332c9d98e9438aad?cid=774b29d4f13844c495f206cafdad9c86';
+    'https://p.scdn.co/mp3-preview/7850aadbb39c36c9e2df41aaeeee3bc5fb915426?cid=af66e62b15fb4bcdbcb5bdf1ead916ba';
 
   let audio = useAudioPlayer(5);
 
@@ -12,10 +12,14 @@ const TestPage = () => {
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <button onClick={() => auth.signin()}></button>
-      <button onClick={() => audio.playSong(testAudio)}>testButton</button>
-      <button onClick={() => audio.pauseSong()}>pause all playBack</button>
+      {/* <button onClick={() => auth.testGetTopTracks()}>
+        Test get top tracks
+      </button> */}
+      {auth.SigninLink}
+      <button onClick={() => audio.playSong(testAudio)}>play audio</button>
+      <button onClick={() => audio.pauseSong()}>pause Song Playback</button>
       <button onClick={() => audio.endSong()}>preemptivly end song</button>
+      <button onClick={() => auth.verifyUser()}> Verify user </button>
     </div>
   );
 };
