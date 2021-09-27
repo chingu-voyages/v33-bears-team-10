efdf35d1a30d7b9e8a4dafa4d8f96b06fd87775f
angelopoole <angelo.m.poole@gmail.com>
2021-09-26 20:24:46 -0400

    Add logic for displaying user's name if they're logged in

--- components/sections/ApplicationHero.tsx
+++ components/sections/ApplicationHero.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import styles from '../../styles/ApplicationHero.module.scss';
 
+import { useAuth } from '../../hooks/useAuth';
+
 const ApplicationHero = () => {
+  const auth = useAuth();
+
   return (
     <div className={styles.applicationCardContainer}>
       <div className={styles.callToAction__container}>
-        <h1 className={styles.callToAction}>Play a game about __you__</h1>
+        <h1 className={styles.callToAction}>
+          Play a game about {auth.user ? auth.user.displayName : '__you__'}
+        </h1>
+
+        {auth.user === null && (
+          <div className={styles.authLink__Container}>{auth.SigninLink}</div>
+        )}
       </div>
     </div>
   );
