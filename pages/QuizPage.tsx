acc8d7ddd9d1fedff29d13917fa9d865ef1cc8e5
Angelo Poole <angelo.m.poole@gmail.com>
2021-10-11 07:48:42 -0400

    init pre-storybook, add instructional comment to QuizPage

--- pages/QuizPage.tsx
+++ pages/QuizPage.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+// import styled from 'styled-components';
 
 const QuizPage = () => {
+  // Take in array of users top songs, return quiz card based off of that.
+  // array of favorite songs must have a url
+
   // will take in the type of game selected.
   return <div>This is the quiz display page!</div>;
 };
