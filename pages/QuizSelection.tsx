c2477f047c42c085052341038ff4c264e4118508
Torja Istiaque <torja50@hotmail.com>
2021-10-15 10:21:31 -0400

    dynamic art component works with backend

--- pages/QuizSelection.tsx
+++ pages/QuizSelection.tsx
@@ -1,34 +1,34 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import QuizCard from '../components/sections/QuizCard';
 import styles from '../styles/QuizSelection.module.scss';
 import axios from 'axios';
-
 const QuizSelection = () => {
   const backendApi = axios.create({
     withCredentials: true,
     baseURL: process.env.NEXT_PUBLIC_BACKEND_API,
   });
+  const [images, setImages] = useState<string[]>([]);
 
-  const getTopTracks = async () => {
+  const fetchTopTracks = async () => {
     try {
-      const res = await backendApi.get('/api/top/tracks');
+      const { data } = await backendApi.get('/api/top/tracks');
 
-      const tracks = res.data.items;
-      const images = [];
+      const tracks = data.items;
+      const imagesFromServer: string[] = [];
       for (const track of tracks) {
-        images.push(track.album.images[0].url);
+        imagesFromServer.push(track.album.images[0].url);
       }
-      //array of 25 objects
-      console.log({ images });
+      setImages(imagesFromServer);
+      console.log(imagesFromServer);
     } catch (error: any) {
       console.error('>>> ERR here', error);
     }
   };
-  const initialImages = getTopTracks();
-  console.log(initialImages);
 
-  // const [images, setImages] = useState(initialImages);
+  useEffect(() => {
+    fetchTopTracks();
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -36,13 +36,13 @@ const QuizSelection = () => {
         <div className={styles.cardFirst}>
           <QuizCard
             text='Play a game based on your playlists!'
-            images={['hello']}
+            images={images}
           />
         </div>
         <div className={styles.cardSecond}>
           <QuizCard
             text='Play a game based on your top tracks!'
-            images={['hello']}
+            images={images}
           />
         </div>
       </main>
